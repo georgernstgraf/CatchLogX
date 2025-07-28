b488be1a5773c8dc054b3e0f9fd368bc0f6b3f9d
AlphaVIE <arman.sarrafi28@gmail.com>
2025-07-28 17:54:23 +0200

    feat: implement Sidebar component with initial structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
+// Alle auskommentierten Sachen werden wieder reaktiviert, sobald Login Form und Funktionalität vorhanden
+
+import DashboardComponent from "@/components/DashboardComponent";
+/* import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react"; */
+
 export default function Home() {
-  return null;
+  /* const session = useSession();
+  const router = useRouter(); */
+
+  /* useEffect(() => {
+    if (!session.data?.user) {
+      router.replace("/login")
+    }
+  }) */
+
+  return <DashboardComponent />;
 }
---
+++ components/Sidebar.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Sidebar = () => {
+
+    type LinkType = {
+        title: "Startseite",
+        path: "/",
+        icon: 
+    }
+
+    const links = [
+        {
+
+        }
+    ]
+
+  return (
+    <div className="w-48 h-screen sticky top-0 border-r-[0.8px] border-r-gray-300 bg-[#f4f4f4] shadow-lg">
+      <div className="border-2 w-full h-full">
+        <div className="flex justify-center">
+          <h1 className="text-center">Universität für Bodenkultur Wien</h1>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
