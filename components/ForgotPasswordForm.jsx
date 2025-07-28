dad95c0420f1bef02835471931dab3c585a777a9
Tadeas Reindl <e12329480@student.tuwien.ac.at>
2025-07-28 19:49:19 +0200

    Center logo in forgot password form header

--- components/ForgotPasswordForm.jsx
+++ components/ForgotPasswordForm.jsx
@@ -28,7 +28,7 @@ const ForgotPasswordForm = () => {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-[#f5f4f2]">
       <div className="bg-[#FCFBF9] rounded-xl shadow-lg flex flex-col items-center p-10 w-[400px]">
-        <div className="flex flex-row items-center gap-9 w-full mb-6">
+        <div className="flex flex-row justify-center items-center gap-9 w-full mb-6">
           <div className="flex items-center h-full">
             <Image src={bokuLogo} alt="Boku Logo" width={150} height={150} className="object-contain" />
           </div>
