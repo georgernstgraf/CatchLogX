f5fc8622dc96a7778f83015f1a8f8559d926b90f
AlphaVIE <arman.sarrafi28@gmail.com>
2025-11-04 14:06:17 +0100

    feat: implement GET endpoint to fetch users and uploads data

---
+++ app/api/admin/fetch-all/route.ts
@@ -0,0 +1,39 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const users = await prisma.user.findMany({
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        name: true,
+        createdAt: true,
+      },
+    });
+    const uploads = await prisma.uploads.findMany({
+      select: {
+        id: true,
+        link: true,
+        state: true,
+        uploaded_by: true,
+        createdAt: true,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        uploads: uploads,
+        users: users,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("[ADMIN] Fehler beim Fetchen der Daten:", error);
+    return NextResponse.json(
+      { error: "Upload- und Nutzerdaten konnten nicht geladen werden" },
+      { status: 500 }
+    );
+  }
+}
